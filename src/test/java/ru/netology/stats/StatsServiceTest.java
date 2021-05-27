586ad02276bbac4955572602612e19b610d47bde
Yudin Egor <yudin.egor26@gmail.com>
2021-05-27 04:52:34 -0400

    edit commit

--- src/test/java/ru/netology/stats/StatsServiceTest.java
+++ src/test/java/ru/netology/stats/StatsServiceTest.java
@@ -6,11 +6,11 @@ import ru.netology.stats.StatsService;
 import static org.junit.jupiter.api.Assertions.*;
 
 class StatsServiceTest {
+    int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
+    StatsService service = new StatsService();
 
     @Test
     void shouldCalculateSumAllSales() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
         int expected = 180;
         int actual = service.sumAllSales(sales);
         assertEquals(expected, actual);
@@ -18,8 +18,6 @@ class StatsServiceTest {
 
     @Test
     void shouldCalculateAverageSales() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
         int expected = 15;
         int actual = service.averageSales(sales);
         assertEquals(expected, actual);
@@ -27,8 +25,6 @@ class StatsServiceTest {
 
     @Test
     void shouldCalculateMaxSalesMonth() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
         int expected = 8;
         int actual = service.maximumSalesAmount(sales);
         assertEquals(expected, actual);
@@ -36,8 +32,6 @@ class StatsServiceTest {
 
     @Test
     void shouldCalculateMinSalesMonth() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
         int expected = 9;
         int actual = service.minSales(sales);
         assertEquals(expected, actual);
@@ -45,8 +39,6 @@ class StatsServiceTest {
 
     @Test
     void shouldCalculateMonthsBelowAverageSales() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
         int expected = 5;
         int actual = service.monthsBelowAverageSales(sales);
         assertEquals(expected, actual);
@@ -54,8 +46,6 @@ class StatsServiceTest {
 
     @Test
     void shouldCalculateMonthsAboveAverageSales() {
-        StatsService service = new StatsService();
-        int[] sales = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};   // Набор данных о продажах  по месяцам
         int expected = 5;
         int actual = service.monthsAboveAverageSales(sales);
         assertEquals(expected, actual);
